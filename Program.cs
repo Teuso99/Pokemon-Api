88f35eb2d31f05ebeea7f27e0e3f54d0348a0f0f
Mateus Machado <mateusmmachado2@gmail.com>
2021-05-23 22:14:31 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PokemonApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://pokeapi.co/api/v2/");
+
+            Console.WriteLine("Nome do Pokemon: ");
+            string pokemon = Console.ReadLine();
+
+            var request = new RestRequest("pokemon/" + pokemon, DataFormat.Json);
+
+            var response = client.Get(request);
+
+            var jObject = JObject.Parse(response.Content);
+
+            string pokemonName = (string)jObject.GetValue("name");
+            int pokemonId = (int)jObject.GetValue("id");
+            double pokemonAltura = (double)jObject.GetValue("height");
+            double pokemonPeso = (double)jObject.GetValue("weight");
+
+
+            Console.WriteLine("Id: " + pokemonId);
+            Console.WriteLine("Nome: " + pokemonName);
+            Console.WriteLine("Altura: " + pokemonAltura / 10 + " m");
+            Console.WriteLine("Peso: " + pokemonPeso / 10 + " kg");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+#Pokemon-Api
+
+----------------
+##About
+This repository is a .NET Console Application that consumes the [PokéAPI](https://pokeapi.co/) for study purposes.
+
+----------------
+##How to Use
+Open the console and navigate to the main folder of the project. Then use command:
+
+```
+dotnet run
+```
+
+Then the program will ask for a Pokemon name and after that will return certain parameters of the response provided by the API.
+
+----------------
+##License
+This project is for study purposes only. Feel free to use.
