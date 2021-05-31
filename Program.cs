0b00a14777ef39634e65d003acded7b8e66e3205
Mateus Machado <mateusmmachado2@gmail.com>
2021-05-30 22:10:10 -0300

    Adiciona Tipos do Pokémon

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace PokemonApi
         {
             var client = new RestClient("https://pokeapi.co/api/v2/");
 
-            Console.WriteLine("Nome do Pokemon: ");
+            Console.WriteLine("Nome do Pokémon: ");
             string pokemon = Console.ReadLine();
 
             var request = new RestRequest("pokemon/" + pokemon, DataFormat.Json);
@@ -25,12 +25,30 @@ namespace PokemonApi
             int pokemonId = (int)jObject.GetValue("id");
             double pokemonAltura = (double)jObject.GetValue("height");
             double pokemonPeso = (double)jObject.GetValue("weight");
+            List<string> pokemonTipos = new List<string>();
 
+            JArray tipos = (JArray)jObject["types"];
 
-            Console.WriteLine("Id: " + pokemonId);
+            foreach (var item in tipos)
+            {
+                pokemonTipos.Add((string)item.SelectToken("type.name"));
+            }
+
+            Console.WriteLine("------------------------------------------------------------");
             Console.WriteLine("Nome: " + pokemonName);
+            Console.WriteLine("Pokédex Id: " + pokemonId);
+            Console.Write("Tipos: ");
+            
+            foreach (var item in pokemonTipos)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
             Console.WriteLine("Altura: " + pokemonAltura / 10 + " m");
             Console.WriteLine("Peso: " + pokemonPeso / 10 + " kg");
+            Console.WriteLine("------------------------------------------------------------");
+            
         }
     }
 }
